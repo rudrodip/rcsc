477b48d511d9d889543735756afea419f26d456d
Rudrodip <official.rudrodipsarker@gmail.com>
2022-05-22 06:56:44 +0600

    added styles to header

--- components/header/header.js
+++ components/header/header.js
@@ -22,23 +22,23 @@ function Header(props) {
   }, [index])
 
   return (
-    <div className='flex flex-row justify-around w-fullflex-wrap' style={{ height: 650 }}>
+    <div className='flex' style={{ height: '70vh' }}>
       <img src={`./background-img/bg${index}.jpg`} className='w-full object-cover opacity-60 blur-[1px]' />
-      <div className='hidden lg:block'>
-        <div className="flex flex-col flex-wrap w-full xl:w-1/3 justify-center lg:items-start overflow-hidden absolute left-48 translate-y-48">
-          <h1 className="my-2 text-3xl md:text-4xl text-white font-bold leading-tight text-center md:text-left">
+      <div className='flex-col md:flex-row'>
+        <div className="flex flex-col flex-wrap xl:w-1/3 items-start overflow-hidden absolute left-4 lg:left-48 translate-y-48">
+          <h1 className="my-2 text-2xl md:text-4xl text-white font-bold leading-tight text-left">
             {props.subtitle}
             <br></br>
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-cyan-300 to-cyan-600 text-8xl">
+            <span className="text-4xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-cyan-300 to-cyan-600">
               {props.title}
             </span>
           </h1>
         </div>
-        <div className='p-10 flex flex-col flex-wrap right-0 translate-y-48 absolute overflow-hidden'>
-          <h1 className="my-2 text-3xl md:text-4xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+        <div className='p-2 left-4 lg:right-4 lg:translate-y-48 absolute overflow-hidden items-start'>
+          <h1 className="my-2 text-xl md:text-4xl text-white opacity-75 font-bold leading-tight text-left md:text-right">
             {props.imageSubtitle}
             <br></br>
-            <span className="text-5xl">
+            <span className="text-xl md:text-5xl">
               {props.imageTitle}
             </span>
           </h1>
