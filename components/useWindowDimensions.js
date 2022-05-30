6f589af8b55adc705d6a9b5682da0705316b56f0
Rudrodip <official.rudrodipsarker@gmail.com>
2022-05-30 23:12:10 +0600

    fixed a major bug in useWindowsDimension

--- components/useWindowDimensions.js
+++ components/useWindowDimensions.js
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
+    const { innerWidth: width, innerHeight: height } = window
+    return {
+      width,
+      height
+    };
 }
 
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState(1980, 1080);
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions(getWindowDimensions())
     }
 
     window.addEventListener('resize', handleResize);
