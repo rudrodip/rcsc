8fe0ac9a505251a367b07c14490d3ecee7d88b37
rudrodip <official.rudrodipsarker@gmail.com>
2022-08-12 15:56:17 +0600

    updated decoding algorithm

--- src/authCode/decode.js
+++ src/authCode/decode.js
@@ -1,30 +1,29 @@
 const decode = (grade, section, roll) => {
-    grade = parseInt(grade)
-    roll = parseInt(roll)
-    
-    function num(x, y){
-        if (y < 100){
-            y = y + x + 100
-        }
-        let output = x**3 + y**2 + 4*x*y
-        output = parseInt(output)
-        return output
+  grade = parseInt(grade);
+  roll = parseInt(roll);
+
+  function num(x, y) {
+    if (y < 100) {
+      y = y + x + 100;
     }
+    let output = x ** 3 + y ** 2 + 4 * x * y;
+    output = parseInt(output);
+    return output;
+  }
 
-    let number = num(grade, roll)
-    let keyCode = {
-        "p" : "jamuna",
-        "j" : "meghna",
-        "m" : "padma"
-    }
+  let number = num(grade, roll);
+  let keyCode = {
+    p: "jamuna",
+    j: "meghna",
+    m: "padma",
+  };
 
-    section = section.toLowerCase()
-    let string = keyCode[section[0]]
-    let sec = string[string.length - 3] + string[string.length - 2]
-    let code = number + sec
+  section = section.toLowerCase();
+  let string = keyCode[section[0]];
+  let sec = string[string.length - 3] + string[string.length - 2];
+  let code = number + sec;
 
-    return code
+  return code;
+};
 
-}
-
-export default decode
+export default decode;
