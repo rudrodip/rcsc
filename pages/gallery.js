bb8dc712db67ab758688e1f1982ea0bbc4d779ab
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-07 21:26:50 +0600

    updated gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Head from 'next/head'
-import Header from '../components/header/header'
 import EventGallery from '../components/eventGallery'
-import useWindowDimensions from '../components/useWindowDimensions'
+import { useState, useCallback } from 'react';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
-function Gallery() {
-  const { width, height } = useWindowDimensions()
+function GalleryPage() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
 
   return (
     <div>
-
       <Head>
         <title>RCSC - Gallery</title>
         <meta name="description" content="Official Website of Rajshahi College Science Club" />
@@ -48,8 +59,74 @@ function Gallery() {
         img5="/background-img/bg5.jpg"
         img6="/background-img/bg2.jpg"
       />
+
+      <div className=''>
+        <Gallery photos={photos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map(x => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
+      </div>
     </div>
   )
 }
 
-export default Gallery
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  }
+];
+
+export default GalleryPage
