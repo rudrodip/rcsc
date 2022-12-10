9a5d88bd6e9c28289a9da81d284d0a11d688f90c
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-10 21:55:13 +0600

    added python member code generator script

---
+++ code_generator.py
@@ -0,0 +1,18 @@
+# main algorithm
+
+# -> take section, roll, class as input
+# -> generates a number based on inputs
+
+import math
+
+def generate_number(x, y, z):
+    pass
+
+def generate_char(number):
+    pass
+
+def generate_code(number, char):
+    pass
+
+def generate_multiple_code(n):
+    pass
