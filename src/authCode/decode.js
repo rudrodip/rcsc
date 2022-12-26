130c9fba68b456110c5020f2e1d4f74ed5086f59
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-26 19:00:43 +0600

    fixed decode algo bug

--- src/authCode/decode.js
+++ src/authCode/decode.js
@@ -1,5 +1,5 @@
 const decode = (encoded, grade, year) => {
-  let key = process.env.NEXT_MEMBER_KEY
+  let key = parseInt(process.env.NEXT_MEMBER_KEY)
   let length = encoded.slice(-1)
   encoded = encoded.slice(length, -1)
   let roll = parseInt(encoded) / (grade * key * (year%100))
