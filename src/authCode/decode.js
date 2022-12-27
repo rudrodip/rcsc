706fd96878c8a4d282f6ed951606410260b6933a
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-27 21:58:39 +0600

    debug

--- src/authCode/decode.js
+++ src/authCode/decode.js
@@ -1,5 +1,8 @@
 const decode = (encoded, grade, year) => {
   let key = parseInt(process.env.NEXT_MEMBER_KEY)
+  console.log(process.env.NEXT_MEMBER_KEY)
+  console.log(typeof(process.env.NEXT_MEMBER_KEY))
+  console.log(key)
   let length = encoded.slice(-1)
   encoded = encoded.slice(length, -1)
   let roll = parseInt(encoded) / (grade * key * (year%100))
