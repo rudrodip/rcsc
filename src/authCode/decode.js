dfa4ce76b493553eae2cf3c17bad040dda28bb5f
rudrodip <official.rudrodipsarker@gmail.com>
2022-12-27 22:35:06 +0600

    fixed issue decode

--- src/authCode/decode.js
+++ src/authCode/decode.js
@@ -1,8 +1,5 @@
 const decode = (encoded, grade, year) => {
-  let key = parseInt(process.env.NEXT_MEMBER_KEY)
-  console.log(process.env.NEXT_MEMBER_KEY)
-  console.log(typeof(process.env.NEXT_MEMBER_KEY))
-  console.log(key)
+  let key = parseInt(process.env.NEXT_PUBLIC_MEMBER_KEY)
   let length = encoded.slice(-1)
   encoded = encoded.slice(length, -1)
   let roll = parseInt(encoded) / (grade * key * (year%100))
