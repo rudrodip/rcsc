8a3bb7613002980c580dd364173e073105c8a417
rudrodip <official.rudrodipsarker@gmail.com>
2023-01-14 14:30:02 +0600

    improved css

--- components/profile/miniProfile.js
+++ components/profile/miniProfile.js
@@ -13,20 +13,19 @@ const Profile = (props) => {
     setScale(1)
   }
   return (
-
-    <div className='p-4 w-1/2 transition ease-in-out duration-500 scale-0' ref={ref} style={{ transform: `scale(${scale})` }}>
+    <div className='p-2 lg:p-4 w-1/2 transition ease-in-out duration-500 scale-0' ref={ref} style={{ transform: `scale(${scale})` }}>
       <div className="h-full flex flex-col lg:flex-row items-center justify-center text-left">
         <Link legacyBehavior href={props.link ? props.link : "#"}>
-          <img alt="team" className="flex-shrink-0 rounded-lg w-36 h-36 md:w-48 md:h-48 object-cover object-center mb-4 lg:mb-0 cursor-pointer" src={props.img ? props.img : "https://dummyimage.com/200x200"} />
+          <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center mb-4 lg:mb-0 cursor-pointer" src={props.img ? props.img : "https://dummyimage.com/200x200"} />
         </Link>
         <div className="flex-grow sm:pl-8">
           <h2 className="title-font font-medium text-lg text-white text-center md:text-left">{props.name}</h2>
-          <h3 className="mb-3 md:text-left text-center text-blue-500">{props.role}</h3>
-          <p className="mb-4 md:text-left text-center">{props.desc}</p>
-          <p className="mb-1">Batch: {props.batch}</p>
-          <p className="mb-1">Current Institution: <span className='text-imp-text-2'>{props.institution}</span></p>
-          <p className="mb-1">Phone: <a href={`tel:${props.number}`} >{props.number}</a></p>
-          <p className="mb-1">Email: <a href={`mailto:${props.mail}`} >{props.mail}</a></p>
+          <h3 className="mb-3 md:text-left text-center text-blue-500 break-word">{props.role}</h3>
+          <p className="mb-4 md:text-left text-center break-word">{props.desc}</p>
+          <p className="mb-1 break-word">Batch: {props.batch}</p>
+          <p className="mb-1 break-word"><span className='text-imp-text-2'>{props.institution}</span></p>
+          <p className="mb-1 break-all">Phone: <a href={`tel:${props.number}`} >{props.number}</a></p>
+          <p className="mb-1 break-all">Email: <a href={`mailto:${props.mail}`} >{props.mail}</a></p>
           <span className="inline-flex">
           </span>
         </div>
