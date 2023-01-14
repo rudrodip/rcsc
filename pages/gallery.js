35f962c238d1697c22601c153771f478f8974f15
rudrodip <official.rudrodipsarker@gmail.com>
2023-01-14 22:07:11 +0600

    updated components

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
-import EventGallery from '../components/eventGallery'
+import { useRouter } from 'next/router'
+import EventCard from '../components/gallery/eventCard'
 
 function GalleryPage() {
+  const router = useRouter()
   return (
     <div>
       <Head>
@@ -21,59 +23,67 @@ function GalleryPage() {
         <meta property="og:image" content="https://i.ibb.co/BKSHpQ9/bg1.jpg" />
         <link rel="icon" href="/logo/rcsc-logo.png" />
       </Head>
-
-      <EventGallery
-        title="Science Fest 2022"
+      <EventCard
+        title="5th Rajshahi College Science Fest 2022"
         desc="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex suscipit, repudiandae voluptatem, rerum placeat excepturi ipsa recusandae consequuntur nam dolor nobis asperiores error corrupti eum cumque blanditiis optio repellendus. Quis incidunt saepe beatae corporis non quasi omnis adipisci sed dolor expedita, ratione dignissimos unde, maxime error corrupti autem suscipit nostrum!"
-        images={photos}
+        photos={[
+          './events/5thRCSF/bg1.jpg',
+          './events/5thRCSF/bg2.jpg',
+          './events/5thRCSF/bg3.jpg',
+          './events/5thRCSF/bg4.jpg',
+          './events/5thRCSF/bg5.jpg',
+          './events/5thRCSF/bg6.jpg']
+        }
+        router={router}
+        url={'/gallery/5th%20RCSF'}
       />
 
-      <EventGallery
-        title="Science Fest 2021"
+      <EventCard
+        title="5th Rajshahi College Science Fest 2022"
         desc="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex suscipit, repudiandae voluptatem, rerum placeat excepturi ipsa recusandae consequuntur nam dolor nobis asperiores error corrupti eum cumque blanditiis optio repellendus. Quis incidunt saepe beatae corporis non quasi omnis adipisci sed dolor expedita, ratione dignissimos unde, maxime error corrupti autem suscipit nostrum!"
-        images={photos}
+        photos={[
+          './events/5thRCSF/bg1.jpg',
+          './events/5thRCSF/bg2.jpg',
+          './events/5thRCSF/bg3.jpg',
+          './events/5thRCSF/bg4.jpg',
+          './events/5thRCSF/bg5.jpg',
+          './events/5thRCSF/bg6.jpg']
+        }
+        router={router}
+        url={'/gallery/5th%20RCSF'}
       />
 
-      <EventGallery
-        title="Science Fest 2021"
+      <EventCard
+        title="5th Rajshahi College Science Fest 2022"
         desc="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex suscipit, repudiandae voluptatem, rerum placeat excepturi ipsa recusandae consequuntur nam dolor nobis asperiores error corrupti eum cumque blanditiis optio repellendus. Quis incidunt saepe beatae corporis non quasi omnis adipisci sed dolor expedita, ratione dignissimos unde, maxime error corrupti autem suscipit nostrum!"
-        images={photos}
+        photos={[
+          './events/5thRCSF/bg1.jpg',
+          './events/5thRCSF/bg2.jpg',
+          './events/5thRCSF/bg3.jpg',
+          './events/5thRCSF/bg4.jpg',
+          './events/5thRCSF/bg5.jpg',
+          './events/5thRCSF/bg6.jpg']
+        }
+        router={router}
+        url={'/gallery/5th%20RCSF'}
+      />
+
+      <EventCard
+        title="5th Rajshahi College Science Fest 2022"
+        desc="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex suscipit, repudiandae voluptatem, rerum placeat excepturi ipsa recusandae consequuntur nam dolor nobis asperiores error corrupti eum cumque blanditiis optio repellendus. Quis incidunt saepe beatae corporis non quasi omnis adipisci sed dolor expedita, ratione dignissimos unde, maxime error corrupti autem suscipit nostrum!"
+        photos={[
+          './events/5thRCSF/bg1.jpg',
+          './events/5thRCSF/bg2.jpg',
+          './events/5thRCSF/bg3.jpg',
+          './events/5thRCSF/bg4.jpg',
+          './events/5thRCSF/bg5.jpg',
+          './events/5thRCSF/bg6.jpg']
+        }
+        router={router}
+        url={'/gallery/5th%20RCSF'}
       />
     </div>
   )
 }
-
-const photos = [
-  {
-    src: "/background-img/bg1.jpg",
-    width: 16,
-    height: 9
-  },
-  {
-    src: "/background-img/bg2.jpg",
-    width: 16,
-    height: 9
-  },
-  {
-    src: "/background-img/bg3.jpg",
-    width: 16,
-    height: 9
-  },
-  {
-    src: "/background-img/bg4.jpg",
-    width: 16,
-    height: 9
-  },
-  {
-    src: "/background-img/bg5.jpg",
-    width: 16,
-    height: 9
-  },
-  {
-    src: "/background-img/bg6.jpg",
-    width: 16,
-    height: 9
-  }
-]
 
 export default GalleryPage
