ed23283589c05b94e91aa30cedf1fa37bbda612f
rudrodip <official.rudrodipsarker@gmail.com>
2023-01-29 18:07:43 +0600

    updated with notifications

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -62,21 +62,24 @@ const Navbar = ({ props, path }) => {
               >
                 <span className="sr-only">Open main menu</span>
                 {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
+                  <div className="indicator">
+                    <span className="animate-pulse indicator-item badge badge-secondary"></span> 
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </div>
                 ) : (
                   <svg
                     className="block h-6 w-6"
@@ -132,12 +135,15 @@ const Navbutton = (props) => {
   return (
     <div>
       <Link legacyBehavior href={props.link}>
-        <a
-          href="#"
-          className={`text-gray-400 hover:bg-gray-700 ${props.classname} block px-4 mx-3 py-2 rounded-md text-base transition duration-150 ease-in-out`}
-        >
-          {props.name}
-        </a>
+        <div className="indicator">
+          {props.name == 'Events' && <span className="animate-bounce indicator-item badge badge-primary">new</span>}
+          <a
+            href="#"
+            className={`text-gray-400 ${props.name == 'Events' && 'bg-gray-700 text-white'} hover:bg-gray-700 ${props.classname} block px-4 mx-3 py-2 rounded-md text-base transition duration-150 ease-in-out`}
+          >
+            {props.name}
+          </a>
+        </div>
       </Link>
     </div>
   )
