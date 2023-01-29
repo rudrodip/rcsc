af3d0017ec43d9d62c10b44f61de9753cd29c99f
rudrodip <official.rudrodipsarker@gmail.com>
2023-01-29 18:07:10 +0600

    updated ui

--- components/segment/segmentCard.js
+++ components/segment/segmentCard.js
@@ -5,10 +5,10 @@ const SegmentCard = ({ name, desc, img, link }) => {
         <div className="card w-96 bg-base-100 shadow-xl image-full m-2">
             <figure><img src={img} alt={name} /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>{desc}</p>
+                <h2 className="card-title text-white">{name}</h2>
+                <p className='text-gray-300'>{desc}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Register</button>
+                    <a className="btn bg-cyan-500 text-white hover:bg-blue-600" href="https://forms.gle/46g3jzSCzXjQdqmn8">Register</a>
                 </div>
             </div>
         </div>
