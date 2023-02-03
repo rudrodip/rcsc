7545012aba75ec8c7f6c6f56d2bc95c41db9853d
rudrodip <official.rudrodipsarker@gmail.com>
2023-02-03 20:57:13 +0600

    updated

--- components/blog/fullBlog.js
+++ components/blog/fullBlog.js
@@ -22,58 +22,49 @@ const FullBlog = ({ blog, userImg, date, author, url, role }) => {
     blog.img
       ? setBlogImg(blog.img)
       : setBlogImg(
-          "https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        );
+        "https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      );
   }, [blog.img]);
 
   return (
     <div>
-      <section className="text-gray-400 bg-gray-900 body-font">
-        <div className="w-full container px-5 py-6 mx-auto flex flex-col">
-          <div className="lg:w-full">
-            <h2 className="font-medium title-font mt-4 text-white lg:text-5xl text-4xl mb-5">
-              {blog?.title}
-            </h2>
-            <h2 className="font-medium title-font mt-4 text-white text-bold text-lg">
-              {`by ${author}`}
-            </h2>
-            <p className="text-base text-gray-300 mb-3">
-              {`${blog?.category}, ${date}`}
-            </p>
-            <div className="rounded-lg overflow-hidden">
-              <img
-                alt="content"
-                className="object-cover object-center h-full w-full"
-                src={blogImg}
-              />
+      <div class="container px-6 mx-auto text-center">
+        {/* <div class="max-w-lg mx-auto">
+          <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl mb-5">{blog?.title}</h1>
+        </div> */}
+        <div class="flex justify-center mt-10">
+          <img class="object-cover w-full h-96 rounded-xl lg:w-4/5" src={blogImg} />
+        </div>
+        <div className="w-20 h-20 inline-flex items-center justify-center text-gray-600 cursor-pointer">
+          <Link href={`/user/${blog?.authorProfile}`}>
+            <div className="p-4 md:p-6 text-center lg:text-left">
+              <div
+                className="block rounded-full shadow-xl mx-auto -mt-16 h-36 w-36 bg-cover bg-center hover:scale-105 transition ease-in-out duration-100"
+                style={{ backgroundImage: `url('${userImg}` }}
+              ></div>
             </div>
-
-            <div className="flex flex-col sm:flex-row mt-10 justify-center">
+          </Link>
+        </div>
+      </div>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="w-full container px-4 pb-6 mx-auto flex flex-col">
+          <div className="lg:w-full">
+            <div className="flex flex-col sm:flex-row justify-center">
               <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                <div className="w-20 h-20 inline-flex items-center justify-center text-gray-600 cursor-pointer">
-                  <Link href={`/user/${blog?.authorProfile}`}>
-                    <div className="p-4 md:p-6 text-center lg:text-left">
-                      <div
-                        className="block rounded-full shadow-xl mx-auto -mt-16 h-36 w-36 bg-cover bg-center hover:scale-105 transition ease-in-out duration-100"
-                        style={{ backgroundImage: `url('${userImg}` }}
-                      ></div>
-                    </div>
-                  </Link>
-                </div>
-
                 <div className="flex flex-col items-center text-center justify-center">
-                  <h2 className="font-medium title-font mt-4 text-white text-bold text-lg">
+                  <h2 className="font-medium title-font text-white text-bold text-lg">
                     {author}
                   </h2>
                   <div className="w-12 h-1 bg-indigo-500 rounded m-2"></div>
-                  <p className="text-base text-white font-bold">{role}</p>
+                  <p className="text-base text-cyan-300 font-bold">{role}</p>
+                  <p className="mb-5 text-gray-300">{`Category: ${blog?.category}, Published: ${date}`}</p>
                 </div>
               </div>
             </div>
           </div>
 
           <div className="w-full sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-left">
-            <h2 className="font-bold title-font mt-4 text-white text-3xl mb-5">
+            <h2 className="font-bold title-font mt-4 text-white text-2xl mb-5">
               {blog?.title}
             </h2>
             {blog?.paragraphs &&
@@ -82,7 +73,7 @@ const FullBlog = ({ blog, userImg, date, author, url, role }) => {
                   <p className="font-normal title-font mt-8 text-cyan-300 text-xl mb-4 opacity-80">
                     {i?.subtitle}
                   </p>
-                  <p className="leading-relaxed text-lg mb-4 whitespace-pre-line break-all">
+                  <p className="leading-relaxed text-lg mb-4 whitespace-pre-line break-word">
                     {i?.content}
                   </p>
                 </React.Fragment>
--- components/blog/miniBlog.js
+++ components/blog/miniBlog.js
@@ -2,48 +2,16 @@
 
 // importing necessary dependencies
 import React from "react";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import Link from "next/link";
 
 // here's the main component
 const MiniBlog = (props) => {
   // props property -->
   // poster -> title -> date -> img
-  const router = useRouter();
-  const [approved, setApproved] = useState(false);
-  const handleClick = (id) => {
-    router.push(`${router.pathname}/${id}`);
-  };
-
-  const deleteHandler = () => {
-    props.select({
-      id: props.id,
-      authorProfile: props.blog?.authorProfile,
-      approved: props.blog?.approved,
-      add: false
-    })
-
-    props.deleteConfirmation()
-  }
-
-  const approveHandler = () => {
-    props.select({
-      id: props.id,
-      authorProfile: props.blog?.authorProfile,
-      approved: props.blog?.approved,
-      add: true
-    })
-
-    props.addConfirmation()
-  }
-
-  useEffect(() => {
-    setApproved(props.blog?.approved);
-  }, [props.blog?.approved]);
 
   return (
     <div className="p-1 sm:p-4 w-1/2 xl:w-1/3 transition ease-in-out duration-500 scale-1">
-      <button>
+      <Link href={`blogs/${props?.id}`}>
         <div className="h-full flex flex-col md:flex-row text-left">
           <img
             alt="team"
@@ -52,10 +20,9 @@ const MiniBlog = (props) => {
               props.blog?.img ||
               "https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             }
-            onClick={() => handleClick(props.id)}
           />
-          <div className="flex flex-col pl-5">
-            <h2 className="title-font font-medium text-xl text-cyan-300 text-left">
+          <div className="flex flex-col pl-0 md:pl-2">
+            <h2 className="title-font font-medium text-md text-cyan-300 text-left break-word">
               {props.blog?.title}
             </h2>
             <h2 className="title-font font-medium text-md text-gray-300 text-left">
@@ -66,32 +33,9 @@ const MiniBlog = (props) => {
             </h3>
             <p className="mb-4 text-gray-300">{props.date}</p>
             <span className="inline-flex"></span>
-
-            <div className="flex items-center mb-4">
-              {props?.editable && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="15"
-                  height="15"
-                  viewBox="0 0 24 24"
-                  className="fill-gray-300 transition ease-in-out duration-150 hover:scale-125 hover:fill-red-500"
-                  onClick={deleteHandler}
-                >
-                  <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
-                </svg>
-              )}
-              {!approved && (
-                <p
-                  className="ml-3 hover:text-blue-400 text-green-400 transition duration-100"
-                  onClick={approveHandler}
-                >
-                  Approve
-                </p>
-              )}
-            </div>
           </div>
         </div>
-      </button>
+      </Link>
     </div>
   );
 };
