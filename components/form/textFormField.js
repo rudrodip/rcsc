924487c2d1846689f23184f23bb102a2d7286865
rudrodip <official.rudrodipsarker@gmail.com>
2023-02-05 13:31:52 +0600

    updated dark mode

--- components/form/textFormField.js
+++ components/form/textFormField.js
@@ -5,7 +5,7 @@ const TextFormField = ({ label, name, placeholder, handleChange }) => {
             <input
                 name={name}
                 type='text'
-                className="block w-full px-4 py-2 mt-2 bg-white border rounded-md dark:bg-gray-800 text-gray-300 border-gray-600 focus:border-blue-500 focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 border rounded-md bg-gray-800 text-gray-300 border-gray-600 focus:border-blue-500 focus:outline-none focus:ring"
                 placeholder={placeholder}
                 onChange={handleChange}
             />
