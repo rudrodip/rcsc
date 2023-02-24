29140204171eaec266e0bb41f2d3cb1b67135d5e
rudrodip <official.rudrodipsarker@gmail.com>
2023-02-24 11:53:41 +0600

    experimental seo

--- pages/_app.js
+++ pages/_app.js
@@ -29,6 +29,24 @@ const MyApp = ({ Component, pageProps }) => {
   }, [router.query, router.events])
 
   return <>
+    <Head>
+      <title>Rajshahi College Science Club</title>
+      <meta name="description" content="Official Website of Rajshahi College Science Club" />
+
+      <meta property="og:url" content="https://rcsc.vercel.app" key="ogurl" />
+      <meta property="og:type" content="website" key="ogtype" />
+      <meta property="og:title" content="Rajshahi College Science Club" key="ogtitle" />
+      <meta property="og:description" content="Official Website of Rajshahi College Science Club" key="ogdesc" />
+      <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/rcsc-web.appspot.com/o/events%2F5th%20RCSF%2Fbg1.jpg?alt=media&token=7f0f6213-9085-48e3-a60b-74d08025ad6d" key="ogimage" />
+
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="rcsc.vercel.app" />
+      <meta property="twitter:url" content="https://rcsc.vercel.app" />
+      <meta name="twitter:title" content="Rajshahi College Science Club" />
+      <meta name="twitter:description" content="Official Website of Rajshahi College Science Club" />
+      <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/rcsc-web.appspot.com/o/events%2F5th%20RCSF%2Fbg1.jpg?alt=media&token=7f0f6213-9085-48e3-a60b-74d08025ad6d" />
+    </Head>
     <AuthContextProvider>
       <LoadingBar
         color='#23b6ed'
