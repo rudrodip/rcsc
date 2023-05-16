5768a53674fd7224130fcdc9ee655f3ce6c8c60c
rudrodip <official.rudrodipsarker@gmail.com>
2023-05-16 07:58:35 +0600

    blanked out for new initialization

--- src/authCode/decode.js
+++
@@ -1,9 +0,0 @@
-const decode = (encoded, grade, year) => {
-  let key = parseInt(process.env.NEXT_PUBLIC_MEMBER_KEY)
-  let length = encoded.slice(-1)
-  encoded = encoded.slice(length, -1)
-  let roll = parseInt(encoded) / (grade * key * (year%100))
-  return roll;
-};
-
-export default decode;
